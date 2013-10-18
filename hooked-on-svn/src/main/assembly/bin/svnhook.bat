c7a168ff3e99a3770608e3c759d6bbcc62ffa1b7
Tom Hombergs <tom.hombergs@gmail.com>
2013-10-18 23:50:22 +0200

    improved batch script

--- hooked-on-svn/src/main/assembly/bin/svnhook.bat
+++ hooked-on-svn/src/main/assembly/bin/svnhook.bat
@@ -1,21 +1,43 @@
 @echo off
 
-if exist "%HOOKED_ON_SVN_HOME%\bin\svnhook.bat" goto setArgs
+REM 1. Set the environment variable %HOOKED_ON_SVN_HOME% to the path where you
+REM    installed the hooked-on-svn distributable.
+
+REM 2. Create a script named "post-commit.bat" in the hooks folder of your SVN repository
+REM    with the following line:
+REM    call %HOOKED_ON_SVN_HOME%/bin/svnhook.bat
+
+set CMD_LINE_ARGS=
+
+set PLUGINS_PATH=%HOOKED_ON_SVN_HOME%/plugins
+set LIB_PATH=%HOOKED_ON_SVN_HOME%/lib
+set BIN_PATH=%HOOKED_ON_SVN_HOME%/bin
+set CLASSPATH=%LIB_PATH%/*;%PLUGINS_PATH%/*;
+set MAIN_CLASS=org.wickedsource.hooked.svn.SvnHook
+
+call %BIN_PATH%/config.bat
+
+echo using classpath %CLASSPATH%
+
+if exist %JAR_PATH% goto jarFound
+echo JAR not found at %JAR_PATH%!
 echo The environment variable HOOKED_ON_SVN_HOME is not defined correctly.
 echo Please point this variable to the path where you installed the SVN Hook.
 goto end
 
-set CMD_LINE_ARGS=
+:jarFound
+echo Found JAR at %JAR_PATH%.
+echo Using plugins found at %PLUGINS_PATH%.
+goto setArgs
+
 :setArgs
 if ""%1""=="""" goto doneSetArgs
 set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
 shift
 goto setArgs
+
 :doneSetArgs
-
-echo %CMD_LINE_ARGS%
-
-:ok
-java -jar %HOOKED_ON_SVN_HOME%/lib/hooked-on-svn-${project.version}.jar %CMD_LINE_ARGS%
+cd %HOOKED_ON_SVN_HOME%
+java -classpath "%CLASSPATH%" %MAIN_CLASS% %CMD_LINE_ARGS%
 
 :end
