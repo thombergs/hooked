31fd84dd607612557ca52c289d35a7a2d599f499
Tom Hombergs <tom.hombergs@gmail.com>
2013-10-20 12:23:30 +0200

    eliminated dependency to airline since we don't need a human usable command line interface

--- hooked-on-svn/src/main/java/org/wickedsource/hooked/svn/SvnHook.java
+++ hooked-on-svn/src/main/java/org/wickedsource/hooked/svn/SvnHook.java
@@ -1,66 +1,61 @@
 package org.wickedsource.hooked.svn;
 
-import io.airlift.command.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wickedsource.hooked.plugins.filedata.notifier.FileMetrics;
 import org.wickedsource.hooked.svn.data.SvnCommitData;
 
-import javax.inject.Inject;
-
 /**
  * @author Tom Hombergs <tom.hombergs@gmail.com>
  */
-@Command(name = "svnhook", description = "Hook for collecting data on SVN checkins and notifying subscribers of " +
-        "the results")
 public class SvnHook {
 
-    private Logger logger = LoggerFactory.getLogger(SvnHook.class);
+    private static Logger logger = LoggerFactory.getLogger(SvnHook.class);
 
-    @Inject
-    private HelpOption help;
-
-    @Option(name = {"-repo"}, description = "Path to the SVN repository root in the local filesystem " +
-            "(parameter REPOS-PATH from SVN hook script).")
     private String repositoryRoot;
 
-    @Option(name = {"-rev"}, description = "Revision number of the checkin that is intercepted " +
-            "(parameter REV from SVN hook script).")
-    private Long revision;
+    private Long revisionNumber;
 
     public static void main(String... args) {
-        SvnHook hook = SingleCommand.singleCommand(SvnHook.class).parse(args);
-
-        if (hook.repositoryRoot == null || hook.revision == null) {
-            System.out.println("Missing command line parameter! See help below.");
-            Help.help(hook.help.commandMetadata);
-            PluginParametersHelp.showHelp();
-            return;
-        }
-
-        if (hook.help.showHelpIfRequested()) {
-            PluginParametersHelp.showHelp();
-            return;
-        }
-
-        try {
-            PluginParametersCollector parameterCollector = new PluginParametersCollector();
-            parameterCollector.collectParameters();
-        } catch (MissingRequiredParameterException e) {
-            System.out.println(String.format("Missing system property -D%s for plugin %s.\n Either provide a value " +
-                    "for this property (see help below) or remove the plugin from the 'plugins' folder.",
-                    e.getParameterName(), e.getPlugin().getClass().getSimpleName()));
-        }
-
+        SvnHook hook = createSvnHook(args);
         hook.run();
+    }
+
+    public SvnHook(String repositoryRoot, Long revisionNumber) {
+        this.repositoryRoot = repositoryRoot;
+        this.revisionNumber = revisionNumber;
+    }
+
+    /**
+     * Creates a fresh SvnHook object from input parameters.
+     */
+    private static SvnHook createSvnHook(String[] args) {
+        if (args.length != 2) {
+            logger.error("SvnHook has been called with the wrong number of parameters! Should be called with path to " +
+                    "local SVN repository as first parameter and SVN revision number as second parameter!");
+            System.exit(-1);
+        }
+
+        String repositoryRoot = args[0];
+        String revisionString = args[1];
+        Long revisionNumber = null;
+        try {
+            revisionNumber = Long.valueOf(revisionString);
+        } catch (NumberFormatException e) {
+            logger.error(String.format("Could not parse revision number '%s'!", revisionString));
+            System.exit(-2);
+        }
+
+        return new SvnHook(repositoryRoot, revisionNumber);
     }
 
     public void run() {
         try {
-            SvnCommitDataCollector collector = new SvnCommitDataCollector(repositoryRoot, revision);
+            SvnCommitDataCollector collector = new SvnCommitDataCollector(repositoryRoot, revisionNumber);
             SvnCommitData data = collector.collectCommitData();
 
-            CollectorPluginVisitor collectorPluginVisitor = new CollectorPluginVisitor(data, repositoryRoot, revision);
+            CollectorPluginVisitor collectorPluginVisitor = new CollectorPluginVisitor(data, repositoryRoot,
+                    revisionNumber);
             FileMetrics metrics = collectorPluginVisitor.visitCollectorPlugins();
 
             // TODO: send data to all notifier plugins
